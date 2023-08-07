04863a4f85611e80a87c2f958ed2116bf58aa2a1
koraykoksal <kksl.koray@gmail.com>
2023-08-07 22:59:38 +0300

    createAsyncThunk

--- src/App/store.js
+++ src/App/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import autReducer from '../features/autSlice'
+import newsReducer from '../features/newsSlice'
+
 
 //! import kısmındaki autReducer bizim verdiğimiz bir isimlendirmedir.
 
 export const store = configureStore({
     reducer:{
         auth:autReducer,
-        //api:newReducer,
+        api:newsReducer,
     },
     devTools: process.env.NODE_ENV !== "production",
 })
--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,12 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+//statelerin tutulduğu alan
 const initialState = {
-    news:[]
+    news:[],
+    loading:false,
+    error:false,
 }
 
-const getNews=()=>{
-    
-}
+export const getNews=createAsyncThunk(
+
+  "getNewsFunc",//? action type name
+
+  async()=>{
+    const API_KEY=`d8eff9e308cd468b9e30910437fd14f1`
+    const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
+
+    const {data} = await axios(url)
+    return
+
+  },
+  
+
+)
+
+
+
+
 
 const newsSlice = createSlice({
   //action creator type
@@ -17,9 +38,25 @@ const newsSlice = createSlice({
         state.news=[]
     },
 
+    //apiden istek atıkdıktan sonra gelen dataya buradan erişiyoruz
+    extraReducers:(builder)=>{
+      builder.addCase(
+        (getNews.pending,(state)=>{
+          state.loading = true
+        })
+      ).addCase(getNews.fulfilled,(state)=>{
+        state.news=action.payload,
+        state.loading = false
+      })
+      .addCase(getNews.rejected,(state)=>{
+        state.error=true,
+        state.loading=false
+      })
+    }
+
   }
 });
 
-export const {} = newsSlice.actions
+export const {clearNews} = newsSlice.actions
 
 export default newsSlice.reducer
